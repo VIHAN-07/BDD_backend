6ab020f2863bad063b32c23531c5065eb564f3ea
ManasGawali <manasmgawali@gmail.com>
2025-03-02 14:24:05 +0530

    timestamp given for donors

--- backend/models/bdd.model.js
+++ backend/models/bdd.model.js
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
+const donorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    reg_number: { type: String, required: true },
+    bloodGroup: {
+        type: String,
+        enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+        required: true
+    },
+    mobile_number: { type: String, required: true },
+    category: {
+        type: String,
+        enum: ["Faculty", "Student"],
+        required: true
+    },
+    approved: { type: Boolean, default: false }
+}, { timestamps: true });
+
 const bddSchema = new mongoose.Schema({
     totalDonors: { type: Number, default: 0 },
     bloodGroups: {
@@ -12,26 +29,7 @@ const bddSchema = new mongoose.Schema({
         "AB+": { type: Number, default: 0 },
         "AB-": { type: Number, default: 0 }
     },
-    recentDonors: [
-        {
-            name: { type: String, required: true },
-            reg_number: { type: String, required: true },
-            bloodGroup: {
-                type: String,
-                enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
-                required: true
-            },
-            mobile_number: { type: String, required: true },
-            category: {
-                type: String,
-                enum: ["Faculty", "Student"],
-                required: true
-            },
-            approved: {type: Boolean, default: false}
-        }
-    ]
-},{
-    timestamps: true
-});
+    recentDonors: [donorSchema]
+}, { timestamps: true });
 
 export default mongoose.model("BDD", bddSchema);
