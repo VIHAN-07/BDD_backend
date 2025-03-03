b82ef064cbff9ede80dee4f85bf24826130c1fbe
Siddhant Sirsat <siddhsirsat123@gmail.com>
2025-03-03 16:38:53 +0530

    login route changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        // const conn = await mongoose.connect("mongodb://127.0.0.1:27017/Blood_Donation?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8");
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error){
         console.error(`Error: ${error.message}`);
